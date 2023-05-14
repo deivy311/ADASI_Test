873bf46fbfd853c5dce019b4da7d0a9c029a9472
david.imbajoa <david.imbajoa@mvm.com.co>
2023-05-14 15:46:07 -0500

    Added point 1

--- point_1/point_1.cpp
+++ point_1/point_1.cpp
@@ -1,77 +1,54 @@
-
-
 #include <gst/gst.h>
-//#include <gst/rtsp-server/rtsp-server.h>
+#include <pipeline_manager.h>
 
-#include <cairo.h>
-#include <cairo-gobject.h>
-int main(int argc, char* argv[]) {
-    GstElement* pipeline, * src, * overlay,* enc, * pay, * sink;
-    GstCaps* caps;
-    GstBus* bus;
-    GstMessage* msg;
-    GMainLoop* loop;
 
-    /* Initialize GStreamer */
-    gst_init(&argc, &argv);
-    loop = g_main_loop_new(NULL, FALSE);
+int main(int argc, char *argv[])
+{
+  GstElement *pipeline;             // Declare GStreamer pipeline
+  GstBus *bus;                      // Declare GStreamer bus
+  GMainLoop *loop;                  // Declare GMainLoop
+  CairoOverlayState *overlay_state; // Declare Cairo overlay state struct
+  pipeline_manager* local_pipeline_manager = new pipeline_manager();
 
-    /* Create elements */
-    pipeline = gst_pipeline_new("mypipeline");
-    src = gst_element_factory_make("videotestsrc", "mysrc");
-    overlay = gst_element_factory_make("textoverlay", "myoverlay");
-    enc = gst_element_factory_make("x264enc", "myenc");
-    pay = gst_element_factory_make("rtph264pay", "mypay");
-    sink = gst_element_factory_make("udpsink", "mysink");
+  /* Initialize GStreamer */
+  gst_init(&argc, &argv);
 
-    /* Set properties */
-    g_object_set(G_OBJECT(pay), "config-interval", 10, NULL);
-    g_object_set(G_OBJECT(pay), "pt", 96, NULL);
-    //g_object_set(G_OBJECT(sink), "protocols", GST_RTSP_LOWER_TRANS_TCP, NULL);
+  /* Create a new GMainLoop */
+  loop = g_main_loop_new(NULL, FALSE);
 
-    g_object_set(G_OBJECT(sink), "host", "127.0.0.1", NULL);
-    g_object_set(G_OBJECT(sink), "port", 5000, NULL);
+  /* Allocate memory for Cairo overlay state struct on heap */
+  overlay_state = g_new0(CairoOverlayState, 1);
 
-    /* Add elements to pipeline */
-    gst_bin_add_many(GST_BIN(pipeline), src, overlay, enc, pay, sink, NULL);
+  /* Set up the GStreamer pipeline */
+  pipeline = local_pipeline_manager->setup_gst_pipeline(overlay_state);
 
-   /* if (!gst_element_link_many(enc, overlay, NULL)) {
-        g_printerr("Failed to link elements\n");
-        return -1;
-    }*/
-    /* Link elements */
-    if (!gst_element_link_many(src, overlay,enc,pay, sink, NULL)) {
-        g_printerr("Failed to link elements\n");
-        return -1;
-    }
-    /* Set up the overlay */
-  /* Set overlay properties */
- /*   g_object_set(G_OBJECT(overlay), "xpos", 100, NULL);
-    g_object_set(G_OBJECT(overlay), "ypos", 100, NULL);
-    g_object_set(G_OBJECT(overlay), "valignment", "center", NULL);
-    g_object_set(G_OBJECT(overlay), "halignment", "center", NULL);
-    g_object_set(G_OBJECT(overlay), "draw", (GstElement*)gst_element_factory_make("textoverlay", "mytextoverlay"), NULL);
-*/
-    g_object_set(G_OBJECT(overlay), "text", "Hello, world!", NULL);
+  /* Start playing the pipeline */
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  g_print("Running...\n");
 
-    /* Start playing */
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    g_print("Running...\n");
-    int messageType = GST_MESSAGE_ERROR | GST_MESSAGE_EOS;
+  /* Get the pipeline's bus */
+  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
 
-    /* Wait until error or EOS */
-    bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-        (GstMessageType)messageType);
+  /* Add a signal watch for the bus */
+  gst_bus_add_signal_watch(bus);
 
-    /* Free resources */
-    if (msg != NULL) {
-        gst_message_unref(msg);
-    }
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
-    g_main_loop_unref(loop);
+  /* Connect the bus's "message" signal to the on_message callback function */
+  g_signal_connect(G_OBJECT(bus), "message", G_CALLBACK(local_pipeline_manager->on_message), loop);
 
-    return 0;
+  /* Unreference the bus */
+  gst_object_unref(GST_OBJECT(bus));
+
+  /* Run the GMainLoop */
+  g_main_loop_run(loop);
+
+  /* Stop the pipeline */
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+
+  /* Unreference the pipeline */
+  gst_object_unref(pipeline);
+
+  /* Free the Cairo overlay state struct */
+  g_free(overlay_state);
+
+  return 0;
 }
