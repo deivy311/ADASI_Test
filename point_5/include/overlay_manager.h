5af0f3a8175b60d41150806df1a854c53b525d0d
david.imbajoa <david.imbajoa@mvm.com.co>
2023-05-14 14:46:57 -0500

    Added overlay Manager

--- point_5/include/overlay_manager.h
+++ point_5/include/overlay_manager.h
@@ -15,7 +15,7 @@ public:
 	overlay_manager();
 	overlay_manager(GstRTSPServer*& server, GstRTSPMountPoints*& mounts, GstRTSPMediaFactory*& factory , string host ,string port, char* file_path);
 	~overlay_manager();
-private:
+protected:
 
 	static void	prepare_overlay(GstElement *overlay, GstCaps *caps, gpointer user_data);
 	static void draw_overlay(GstElement *overlay, cairo_t *cr, guint64 timestamp,
